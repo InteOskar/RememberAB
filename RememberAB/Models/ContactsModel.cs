14832292d6e59e7aa945e34ad7d7ec0c833f7a9f
Oskar Wennerlund <oskarwennerlund@hotmail.com>
2021-03-10 16:00:41 +0100

    RememberAB

---
+++ RememberAB/Models/ContactsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RememberAB.Models
+{
+    public class ContactsModel
+    {
+        [Key]
+        private int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
