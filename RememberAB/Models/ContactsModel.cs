7b7b4f1ddcfeae4ac83192b5047a960f768c58af
Simon Karvonen <simonoskarkarvonen@gmail.com>
2021-03-10 16:38:28 +0100

    Fixat lite valideringar

--- RememberAB/Models/ContactsModel.cs
+++ RememberAB/Models/ContactsModel.cs
@@ -11,12 +11,21 @@ namespace RememberAB.Models
         [Key]
         private int Id { get; set; }
         [Required]
+        [StringLength(20, MinimumLength = 1)]
+        public string UserName { get; set; }
+        [Required]
         public string FirstName { get; set; }
         [Required]
         public string Surname { get; set; }
         [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+        ApplyFormatInEditMode = true)]
+        [Range(0, 200)]
         public DateTime BirthDate { get; set; }
         [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 8)]
+        public string Password { get; set; }
     }
 }
